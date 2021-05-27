8ab5d193883968d83657d0f7318d3e878ec0c654
Mzying2001 <Mzying2001@qq.com>
2021-05-27 13:46:15 +0800

    Add Settings

---
+++ SimpleCalculator/Models/Settings.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleCalculator.Models
+{
+    class Settings
+    {
+        public bool RestoreResultsAtStartup { get; set; }
+
+        public static Settings DefaultSettings
+        {
+            get => new Settings()
+            {
+                RestoreResultsAtStartup = true,
+            };
+        }
+    }
+}
